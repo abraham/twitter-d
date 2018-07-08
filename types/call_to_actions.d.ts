7e76ccbefe1d04e0d638a409a55eb1f3d4d06153
Abraham Williams <4braham@gmail.com>
2018-07-07 21:21:23 -0500

    Add WatchNow

--- types/call_to_actions.d.ts
+++ types/call_to_actions.d.ts
@@ -1,5 +1,7 @@
 import { VisitSite } from './visit_site';
+import { WatchNow } from './watch_now';
 
 export interface CallToActions {
-  visit_site: VisitSite;
+  visit_site?: VisitSite | null;
+  watch_now?: WatchNow | null;
 }
